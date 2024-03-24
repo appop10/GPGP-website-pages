c84ead8cee668f5b348d285aa7aeee0194dce686
appop10 <appoplawski10@gmail.com>
2024-03-23 19:09:35 -0500

    tables with each individual day's schedule expand and contract when the minus sign is clicked

--- js-files/eventFunctions.js
+++ js-files/eventFunctions.js
@@ -1,10 +1,22 @@
 window.addEventListener('load', function() {
-
     let arrows = this.document.querySelectorAll('.arrow');
+    let tableContainers = this.document.querySelectorAll('.table-container');
 
     arrows.forEach((arrow) => {
         arrow.onclick = () => {
+            let arrowIndex = findIndex(arrow.id);
+            let tableContainer = tableContainers[arrowIndex - 1];
+
             arrow.children[1].classList.toggle('turn-vertical');
+            tableContainer.classList.toggle('expand-table');
         }
     });
 });
+
+function findIndex(arrowId) {
+    let index = arrowId
+    .split('')
+    .filter((char) => parseFloat(char));
+
+    return index.join('');
+}
